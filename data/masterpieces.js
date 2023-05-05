e56876b3d3f512f02738d05530171c2febf5e6a7
JstVex <heinhtetlulu14@gmail.com>
2023-05-06 02:43:56 +0630

    Add each artwork inside an array

---
+++ data/masterpieces.js
@@ -0,0 +1,32 @@
+export const masterpieces = [
+    {
+        title: "Azula",
+        description: "Traditional artwork plus digital art and effects of azula from avatar the last air bender.",
+        image: "https://dsm01pap007files.storage.live.com/y4muLwR8GfZnoE9pXhnszIIz_mEQQPzu2NwurQGlTC-irZ6Ysm4RnHUJhtXTeoUzZYd4Nsau0OfURH6_IJoehiuPdowwgXgA-B_lxqPXNJgMcIhwdDRBH5k89d9d96kKsbQpGIoxw8sqI8cEbtMAw0MDXfNY2ICQor0fGFuuFnw2RkJI24IG6rlSZgJ7liLiD7N?width=960&height=1280&cropmode=none",
+        alt: "Azula masterpiece ><",
+        startDate: "21 Apr",
+        finishedDate: "24 Apr 2023",
+        comment: "This is the best for both traditional and digital art I have ever seen. I mean just look at all those highlights and tones, they really bring Azula to lifeee. Such amazing talent. I can't so much with thissss babeee ><.  ",
+        by: "ur hhll ^^"
+    },
+    {
+        title: "The Heydar Aliyev Center (First Desmos Project)",
+        description: "Recreating the famous building of Zaha Hadid, The Heydar Aliyev Center on desmos using over 900 equations.",
+        image: "https://dsm01pap007files.storage.live.com/y4mRAgq4gXvmqSzSKLbJa_35e7AYuR5mMB3ZDM4OissQsvwsWAxfX-98VgHdVlwhSa5Xai1AddyY084Bsh7LW_4jxy4o_6C1MUyFqro6iAntLoO9_1CbQsLgx7N0o_ztfeszXtTuscPqS5NQOKNhYb__q6kKcPh9XcOSTLSbOxhWlFWwRms_CahSUa4IGeDhWPr?width=1536&height=960&cropmode=none",
+        alt: 'Desmos masterpiece ><',
+        startDate: "2 Jan 2023",
+        finishedDate: "3 May 2023",
+        comment: "I HAVE NEVER SEEN A MORE TALENTED INDIVIDUAL THAN THIS PRODIGY EAPM RIGHT HERE. I MEAN WHO IS ABLE TO CREATE SUCH A MASTERPIECE USING ONLY EQUATIONS??? JUST LOOK AT THOSE DETAILSSS. I JUST CANT. SPEECHLESS.",
+        by: "ur lulu ^^"
+    },
+    {
+        title: "First project on exterior landscaping",
+        description: "First project on exterior landscaping.",
+        image: "https://dsm01pap007files.storage.live.com/y4myOuSouR_fROn1GJnXXPXIh0FgMB-1I7jPJk8gyBZjAZoTu_V1gfc6y8v8GE2cBzOV78mgpX45RqK1a4sCpfUePlFYiXeBIhBbLCZU4X_7mhNM28uGyuBHorg2qMrnc3zl7P4fF1ofGINqw3PwdaVjRS9PYk8TH5_MRIb6ZUZb4_Qocj6xxH1P0kBLsbkAer0?width=1280&height=960&cropmode=none",
+        alt: 'Landscape masterpiece ><',
+        startDate: "4 Apr",
+        finishedDate: "3 May 2023",
+        comment: "Its sooooo professional and detailed and cute and eu soat. Theres no way this is the first project omg and first time coloring with markerr??? Those details everywhere. Bringing depth and texture to a plain paper and making it so perfect is no ordinary talent. Another masterpiece from u my boo boo ^^",
+        by: "ur boo boo ^^"
+    }
+]
