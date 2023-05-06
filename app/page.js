198654b0f867218e09edeb7696ea613ea04f5b73
JstVex <heinhtetlulu14@gmail.com>
2023-05-06 23:17:27 +0630

    Structure and add choice functionality

--- app/page.js
+++ app/page.js
@@ -1,10 +1,120 @@
+'use client'
+
+import { useState, useRef } from 'react';
 import styles from './page.module.css';
+import { AiFillHeart } from "react-icons/ai"
+import { BsArrowLeft, BsArrowRight } from "react-icons/bs";
+import Link from 'next/link';
 
 export default function Home() {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const group1Ref = useRef(null);
+  const group2Ref = useRef(null);
+  const group3Ref = useRef(null);
+  const group4Ref = useRef(null);
+
+  const url = ['/happianni', '/dates', '/mgcharcoal', '/masterpieces'];
+
+  const handleLoveClick = () => {
+    const nextIndex = activeIndex + 1 <= 3 ? activeIndex + 1 : 0;
+
+    let currentGroup, nextGroup;
+    if (activeIndex === 0) {
+      currentGroup = group1Ref.current;
+      nextGroup = group2Ref.current;
+    } else if (activeIndex === 1) {
+      currentGroup = group2Ref.current;
+      nextGroup = group3Ref.current;
+    } else if (activeIndex === 2) {
+      currentGroup = group3Ref.current;
+      nextGroup = group4Ref.current;
+    }
+    else {
+      currentGroup = group4Ref.current;
+      nextGroup = group1Ref.current;
+    }
+
+
+    if (currentGroup && currentGroup.dataset) {
+      currentGroup.dataset.status = "after";
+    }
+    if (nextGroup && nextGroup.dataset) {
+      nextGroup.dataset.status = "becoming-active-from-before";
+    }
+
+    setTimeout(() => {
+      if (nextGroup && nextGroup.dataset) {
+        nextGroup.dataset.status = "active";
+      }
+      setActiveIndex(nextIndex);
+    });
+  }
+
+  const handleHateClick = () => {
+    const nextIndex = activeIndex - 1 >= 0 ? activeIndex - 1 : 3;
+
+    let currentGroup, nextGroup;
+    if (activeIndex === 0) {
+      currentGroup = group1Ref.current;
+      nextGroup = group4Ref.current;
+    } else if (activeIndex === 1) {
+      currentGroup = group2Ref.current;
+      nextGroup = group1Ref.current;
+    } else if (activeIndex === 2) {
+      currentGroup = group3Ref.current;
+      nextGroup = group2Ref.current;
+    }
+    else {
+      currentGroup = group4Ref.current;
+      nextGroup = group3Ref.current;
+    }
+
+    if (currentGroup && currentGroup.dataset) {
+      currentGroup.dataset.status = "before";
+    }
+    if (nextGroup && nextGroup.dataset) {
+      nextGroup.dataset.status = "becoming-active-from-after";
+    }
+
+    setTimeout(() => {
+      if (nextGroup && nextGroup.dataset) {
+        nextGroup.dataset.status = "active";
+      }
+      setActiveIndex(nextIndex);
+    });
+  }
+
   return (
     <main className={styles.container}>
       <div className={styles.card_groups}>
-        <div className={styles.card_group} >
+        <div className={styles.card_group} ref={group1Ref} data-index="0" data-status="active">
+          <div className={styles.little_card}>
+
+          </div>
+          <div className={styles.big_card}>
+
+          </div>
+          <div className={styles.little_card}>
+
+          </div>
+          <div className={styles.big_card}>
+
+          </div>
+          <div className={styles.little_card}>
+
+          </div>
+          <div className={styles.big_card}>
+
+          </div>
+          <div className={styles.little_card}>
+
+          </div>
+          <div className={styles.big_card}>
+
+          </div>
+
+        </div>
+        <div className={styles.card_group} ref={group2Ref} data-index="1" data-status="unknown">
           <div className={styles.little_card}>
 
           </div>
@@ -30,8 +140,7 @@ export default function Home() {
 
           </div>
         </div>
-
-        {/* <div className={styles.card_group}>
+        <div className={styles.card_group} ref={group3Ref} data-index="2" data-status="unknown">
           <div className={styles.little_card}>
 
           </div>
@@ -57,7 +166,7 @@ export default function Home() {
 
           </div>
         </div>
-        <div className={styles.card_group}>
+        <div className={styles.card_group} ref={group4Ref} data-index="3" data-status="unknown">
           <div className={styles.little_card}>
 
           </div>
@@ -82,7 +191,23 @@ export default function Home() {
           <div className={styles.big_card}>
 
           </div>
-        </div> */}
+        </div>
+      </div>
+
+      <div className={styles.card_swiper_buttons}>
+        <button className={styles.arrow_button}>
+          <BsArrowLeft className={styles.arrow_icon} onClick={handleHateClick} />
+        </button>
+        <button className={styles.heart_button}>
+          <Link href={url[activeIndex]}>
+            <AiFillHeart className={styles.heart_icon} />
+          </Link>
+
+        </button>
+        <button className={styles.arrow_button}>
+          <BsArrowRight className={styles.arrow_icon} onClick={handleLoveClick} />
+        </button>
+
       </div>
     </main >
   )
