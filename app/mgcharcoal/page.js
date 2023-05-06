c02d1533ecca79360e84225c364b145e6741468e
JstVex <heinhtetlulu14@gmail.com>
2023-05-06 19:36:56 +0630

    Structure and style mg charcoal lyy page

---
+++ app/mgcharcoal/page.js
@@ -0,0 +1,119 @@
+import styles from './mgcharcoal.module.css'
+import { BsArrowDown } from 'react-icons/bs'
+import { AiOutlineArrowLeft } from 'react-icons/ai'
+import Link from 'next/link'
+
+export default function MgCharcoal() {
+    return (
+        <main className={styles.container}>
+            <div className={styles.back}>
+                <AiOutlineArrowLeft />
+                <Link href='/' >
+                    back
+                </Link>
+            </div>
+            <section className={styles.firstsection}>
+                <div className={styles.title}>
+                    Guess what this section is about my boo boo &gt;&lt;
+                </div>
+                <div className={styles.subtitle}>
+                    I will give u a hint. Its about something veri eu soat and precious and also which u lob veri much ^^
+                </div>
+                <BsArrowDown className={styles.downarrow} />
+            </section>
+            <section className={styles.secondsection}>
+                <div className={styles.title2}>
+                    Yesso its about mg charcoal lyy boo boo ^^
+                </div>
+                <div className={styles.subtitle2}>
+                    Just him babe hes just here to seek attention like he always does with his eu soat face:(
+                </div>
+                <div className={styles.images}>
+                    <img src="https://dsm01pap007files.storage.live.com/y4mf54Qkr_rRO7hqgwTArbEzY878zqrdOLn031wxX7yzTv89KLuwH7det91wkMYoSOdtLaBpiRvl6NZ3mWd01AzEA8eE0UaHnCLE2ayMgP-p10if1pwHAbLS2_4Nx9441u9bB9DHQS165xRNTBu7W5e0ivyKdG3_DqMpibgKOaTp3n8ifD5ITudEHOjH2XeWFn3?width=722&height=722&cropmode=none" alt="" className={styles.first_img} />
+                    <img src="https://dsm01pap007files.storage.live.com/y4mj33s8WS69mP71filUY6mbZZde27r2mTn5okPHLi8oVEN_l4Eo09r_-dDvz30f4DVWjNVC5b264i_8b_X3ffrMKN3YDIVNQBwqZut6pUTz0V_Kwd-UBQbmTyaavVhaZo8Xk1OEeTFZ0b9SZhedtDacAO2Jm5bFHVydsi2LfVRWL_suzUhKuOF5dPaepK-Rvri?width=436&height=434&cropmode=none" alt="" className={styles.center_img} />
+                    <img src="https://dsm01pap007files.storage.live.com/y4mEG7jxI7M2jQkojSrO_t5cDT9pu77phNaIQBitruoHDbkPwve6KqrVy36hvAMAgBhZ1kDQDz08kleBWOwk5xBkle7geD7c1H34tbC6C-f4VRG_yPsbBDsFEHwwmH6VZs_9v9pctZhrka4ECqjea0eixq7MMJuoBfYS64bJ3gFsvhLq1qW5up200K8UBPK9WVQ?width=762&height=706&cropmode=none" alt="" className={styles.last_img} />
+                </div>
+            </section>
+            <section className={styles.thirdsection}>
+                <div className={styles.head}>
+                    <img src="https://dsm01pap007files.storage.live.com/y4mbyHzTh74JsdJhvVgjHyfNp4lPSV7pGq1_8L3v8ovIQ-rP6Yo4UkBEjprz683eXI_MR5Li0Jih8Mi9_Kp29ozXZuwiqf3h8OcTf1TEcCfh6cfNVaG1gtrGA-DsC04Ultk_mudPcy6iZxJafVKqM6pkoyB3vYIXC-4inFIpctWflqRf0q-HQHFLjSinsSLBqAZ?width=1304&height=962&cropmode=none" alt="" className={styles.catrac_img} />
+                    <div className={styles.catrac_text}>
+                        head
+                    </div>
+                </div>
+                <div className={styles.body}>
+                    <img src="https://dsm01pap007files.storage.live.com/y4mmGH8vDX_Tfjt68wBRopS65p_oLvdb9ZdCG6TsNddxbsET5MBo745cFJL85j7SVqp0w8qn-WrQIX5Bt_3Asxq1oale_0ZvYRt1WzIbPVRmTphYR8uqKif5awEFv_mVYK-5SDPsVM5PYp1ClAn9EoYKZVZF9i0UneHu5EKJCIvvqt_c5KWm_JqNRtOY5uK1LRv?width=720&height=960&cropmode=none" alt="" className={styles.catrac_img} />
+                    <div className={styles.catrac_text}>
+                        body
+                    </div>
+                </div>
+                <div className={styles.face}>
+                    <img src="https://dsm01pap007files.storage.live.com/y4mFzXQUxAHmnu_WXMdWPFJ8eI8QZEEJMFZP7SkujwZkF4jFVyaie7OPFeLrL2Lx5VDBD0vOe9_Xcha-edqIDD8gD_xXv8Cz6lIb2CR2qSy6TepB3N7BcJlvW4mx0kUa7FxfNHOkvX4fyqk96L0agV5Tp0b31rWm4nLOpsSNkoXA1UsE0yJcO7vhG2hQWxYqy-d?width=194&height=188&cropmode=none" alt="" className={styles.catrac_img} />
+                    <div className={styles.catrac_text_face}>
+                        face n M
+                    </div>
+                </div>
+                <div className={styles.inside}>
+                    <img src="https://dsm01pap007files.storage.live.com/y4m0rAopFXPMgJnwWPjzR8z32-BlDg5EmxMelkxRxmESpdC9ZNkMUkDxT_Jxi1a_T256FGqLEOlOPlvOMsrE5Uyw6z5SYK2LQn2Ax7kOOyF5gGmXYioBhqqkfYQ1Xau4hE1IU4evlAv5ivWvMyLc5nxVkYcUCwbqp3OGJv11THssMs_Cq6Qtc2ZzUQp4OmhkSgY?width=454&height=598&cropmode=none" alt="" className={styles.catrac_img} />
+                    <div className={styles.catrac_text_atwinnthrr}>
+                        a twinn thrr
+                    </div>
+                </div>
+                <div className={styles.title3}>
+                    Hes now showing off his charcoaly catrateristics ^^
+                </div>
+                <div className={styles.subtitle3}>
+                    Just look at all of his catrateristics babe all so eu soat n soft n precious nw:( n n hes a male kalay babe an eu soat thrr thrr male kalay my boo boo:((
+                </div>
+                <div className={styles.hand}>
+                    <img src="https://dsm01pap007files.storage.live.com/y4mbPbQR0wpdhgzesJA6Rof_FJFkcS8TmiI0UsaySiroAtkznxIoVSM4AXUeRSN1GXJmK_BUvpBtmo2mZYaj1a1CntMv4PJwdxjskP9Ga1EperMszl0DyELdbV53Ruwh-ooOv1IX6Xpl7B_HC5nt1wOayMppZOB71uQlnSCouu-Pvbia82wWhM7UvE13pwc7r16?width=714&height=964&cropmode=none" alt="" className={styles.catrac_img} />
+                    <div className={styles.catrac_text}>
+                        hand
+                    </div>
+                </div>
+                <div className={styles.thigh}>
+                    <img src="https://dsm01pap007files.storage.live.com/y4m6ze4V-6wGfbHBCw8uOTH27pZvwqdjNuwm0pxTF1vCx2DRYdtlW4-6DniBGleqQbKnmp-VOa6v_fAD3YbOOMW33hvoAt-3S8uJr456DJ6MNoOh9RFXG8CXv3LAUyEiVgRBaDQ3Hacgx4Ih0NlQXOPDgrVLV1XRhAED6XCFsGXMKAt55AYYCRmrH_wlu5LwLOW?width=280&height=466&cropmode=none" alt="" className={styles.catrac_img} />
+                    <div className={styles.catrac_text}>
+                        thigh
+                    </div>
+                </div>
+                <div className={styles.arm}>
+                    <img src="https://dsm01pap007files.storage.live.com/y4m9Rj-JTYBUV3hEkC4mGN3Pdmaj-3YifPvrI5TZefeycjmEVwPzB57fqAJoy3D5mkn54oqT0o3f7Eu0YLa4PU7Tc0jZbTflRLYHhADKNjcMXlFJWcI8frRjofdWM1UUEcNgdZlSURjq9Tk9zX3bG5HEMPwXH3Ay_F4wnun-3kD6mBovlnm63xfZsElqvQn6M-u?width=402&height=404&cropmode=none" alt="" className={styles.catrac_img} />
+                    <div className={styles.catrac_text}>
+                        arm
+                    </div>
+                </div>
+                <div className={styles.tail}>
+                    <img src="https://dsm01pap007files.storage.live.com/y4mTCRz0alAYf4t3zBGnkzo2NTHvul3H6TjMsuMAsr9N1ksYQy9NTDv01KmS8BtZAb0BOfP_iy5Wte_dfIWqf8tmvd6AEVs8oQtoT9tHtQU43Zetr0DYpbmAOMAZZo8IBYHsB8cISy8ct3IVZsC4rmI3gHsS7hp-NgKG4izCqjJwGde2BNcdfpDMFxuZ12CkHEe?width=450&height=388&cropmode=none" alt="" className={styles.catrac_img} />
+                    <div className={styles.catrac_text}>
+                        tail
+                    </div>
+                </div>
+            </section>
+            <section className={styles.forthsection}>
+                <div className={styles.title4}>
+                    Now hes outside ^^
+                </div>
+                <div className={styles.subtitle4}>
+                    He says he wants to play with u in the garden my boo boo n he says he lobs staying outside ^^
+                </div>
+                <img src="https://dsm01pap007files.storage.live.com/y4mDdtpyToc2oCffFVttiPbcViL_jcTAavrxC5tdrE-h9xarqV3quSS1dzD4zif0OqXqfvfAD_BevSXgz2ZgMcj72dvX4xRq2b21NBUvkqjZEWa--J52bzU_2WsFwpbPnuGzn38V94oz0a-kuS5NwohWxOJtxWNllyJXZQAF5R0HmAJ2hdpUZd455H32DHypgrW?width=720&height=960&cropmode=none" alt="" className={styles.garden1} />
+                <img src="https://dsm01pap007files.storage.live.com/y4m1iL6pfSMRJxovOjhO85xSPtiah9V4vAStXJXfYoiUYQ7UNKJvTkpZ_COr-5c_4NkTzh_vBNJMI4nW9QhYH8h5pBss-lpxQ2SIns2Aw_65COzblzwRXXiOVtwFyQSvyJLRLIYpV8Z4A9KMjqzKrPBPMYiEGisYiaeUBLKzPfup7HFQbsrhtoFZkiSGOIjK2Qr?width=724&height=964&cropmode=none" alt="" className={styles.garden2} />
+                <img src="https://dsm01pap007files.storage.live.com/y4mR59gMEUDueFNBXm-LyiWrd_sqJKt4nYzkP5SusvJm5eqoND2N9J0ACuFJqGd7q-WkiIfgUJG6oglzYFnuXt9z8HdqEgvUZa_pESpelqiiDUPg3Az_Ny99gPfNhgw7pKj7SCgXVBmDPXp2OneL_RdeZ43ml3uOSZqzLnzMOqkAWPYCrV5wh8qPTMUUQvqijO9?width=720&height=960&cropmode=none" alt="" className={styles.garden3} />
+                <img src="https://dsm01pap007files.storage.live.com/y4mHUy6DW_wNXXZw_p7fa159qs4mE44W7mfhWJRk9CSm6shsPmkS9rzSbWwb1-d1rkA1uXY_uJiofgUJWmrG61v1bZWMWrzhxVWifuy6Pi2q9XIvYdNzviBH0YWNpTRC-SZVWJLcI5uzDiPk8TVS7X1UsrqNrue3OOkRs4PkvsFCyuP4kB6YmgDEm16eq7DXIlY?width=726&height=968&cropmode=none" alt="" className={styles.garden4} />
+            </section>
+            <section className={styles.fifthsection}>
+                <div className={styles.title5}>
+                    Now hes in ur room to chwl u ^^
+                </div>
+                <div className={styles.subtitle5}>
+                    He is here to stay beside u but he felt aslweep so he says good nite beforehand ^^
+                </div>
+                <img src="https://dsm01pap007files.storage.live.com/y4mmqY5W-7BPKEkLdMMOF-0BXA1PfVgItJqV1v-6tZTVb_FBMsKlEqWkgBsFcsXsowfgAP66jtHUEsi0GdjXZGZt7DhHMdN64H-n4rcJmiOOJzYv7Q3choUUoHJnCCExYk4z-z7EjYqgjzEH1NqCBX_GdH5_d_hTzMeekKZNYjxtYZbnTbHec8fDb95IKR5MgIN?width=722&height=962&cropmode=none" alt="" className={styles.sleep1} />
+                <img src="https://dsm01pap007files.storage.live.com/y4mXlFjAdNHyf3AjO4J6fUJxUpMgiw4m0iO1ofOXmTQauIWgtPj1ROczDiwEfa7KD4cAYLf0CSBM6cmlk-QDuebldy-uuIZxcMxBM08MFslLE8XuQeNpeI_k6ZP9Jzn45tX_ZVi9QvEQq0iJci0p7vpd9_o346NHXB_8rUe51b0fri4TucuXWHefSrWIcDjRsFU?width=720&height=970&cropmode=none" alt="" className={styles.sleep2} />
+                <img src="https://dsm01pap007files.storage.live.com/y4mYc5WjS55JfL12n6-H_F6aVe6LjLz3IlPDeLuhfqQgOiEbuhBW1YjDfiiYetPeYdAz-rR34Bdo1slQMhlKW-xR7WqqLbFcueJyWugYctBxZNdnmC9HynMvpJ1TAtrAwdKPQ4tHJ5Mv7a44-FVtykI81Gt5Qk7G3t2qSaHZvRSTDJmpMlukXqx9FL90cD8lTvR?width=716&height=962&cropmode=none" alt="" className={styles.sleep3} />
+                <img src="https://dsm01pap007files.storage.live.com/y4myrTA193VIOZpAAJ6EkkDjwId9uLFy-AbES68MSYGKxBtpi9JOjtOCEs2jKLwtx1ZZSqJX2c7F70GTvFvdnfHA6DhEzHXnyCj-CPMZxXeWE9EIu7CiDm9qEIcEH33w2eaxIpuparx1FcLwpWU2znVO7cdNkLGSJfvQswPupqfU0BC2eO6nbjjVY396iiTDKMe?width=720&height=960&cropmode=none" alt="" className={styles.sleep4} />
+            </section>
+        </main>
+    )
+}
