546e5506a6aa2b1b99d9b20e5677ba4ff12fb52e
JstVex <heinhtetlulu14@gmail.com>
2023-05-08 20:18:52 +0630

    Add more data

--- data/masterpieces.js
+++ data/masterpieces.js
@@ -24,9 +24,39 @@ export const masterpieces = [
         description: "First project on exterior landscaping.",
         image: "https://dsm01pap007files.storage.live.com/y4myOuSouR_fROn1GJnXXPXIh0FgMB-1I7jPJk8gyBZjAZoTu_V1gfc6y8v8GE2cBzOV78mgpX45RqK1a4sCpfUePlFYiXeBIhBbLCZU4X_7mhNM28uGyuBHorg2qMrnc3zl7P4fF1ofGINqw3PwdaVjRS9PYk8TH5_MRIb6ZUZb4_Qocj6xxH1P0kBLsbkAer0?width=1280&height=960&cropmode=none",
         alt: 'Landscape masterpiece ><',
-        startDate: "4 Apr",
+        startDate: "29 Apr",
         finishedDate: "3 May 2023",
         comment: "Its sooooo professional and detailed and cute and eu soat. Theres no way this is the first project omg and first time coloring with markerr??? Those details everywhere. Bringing depth and texture to a plain paper and making it so perfect is no ordinary talent. Another masterpiece from u my boo boo ^^",
         by: "ur boo boo ^^"
-    }
+    },
+    {
+        title: "Bubble Doggo",
+        description: "First drawing using colorpencils on black paper",
+        image: "https://dsm01pap007files.storage.live.com/y4mLrHd06Q6VPxedmgV_gMfkgOiCYI5p8vSdTYKjXA4YMQ2SmrxsMRg-n1AUjCY1dcD3QssEizXhERe65ZpIa0OU7QuXkKk1wHAp8mHleRrJ9R-aewr93vIRlSuSisw9rLuX1mv-TcYQUPKilVFMjA61jkGPDfk3PPhvCkHsuMUUcya3baCKMS0spjPzWXQdCR1?width=722&height=962&cropmode=none",
+        alt: 'Bubble Doggo masterpiece',
+        startDate: "30 Oct",
+        finishedDate: "2 Nov 2022",
+        comment: "Omgggggg its bubble doggoooo :333333. He looks so realistic and happi ^^. Just look at all those bubbles like whaaaa ><. Oh oh n its on booko which i bought for this talented eapm lyy :333",
+        by: "ur koko ^^"
+    },
+    {
+        title: "The love of my life",
+        description: "Portrait drawing of my boyfriend 'Hein Htet Lu Lu' aka the boy I love the most.",
+        image: "https://dsm01pap007files.storage.live.com/y4m2yFGUrGf-yOd5tCqBZTw5IUx8VhQ6LjwWliy9HpixGk6GlgwUyw-Twpu_d3S1yMRNjXIqqX7Gh_MKXpsWlikbhIUEyDtQv0jLhAMXPuS1-jaTAhrzlp3R3SzCedjZIVpYl-RjrBZ8WI-2FTgSX4Rtzl5lbwRLV7rydldr3ZIR35EPKkqTfqY7R6R4S4eBvGL?width=1034&height=1382&cropmode=none",
+        alt: 'Portrait masterpiece of meo ^^',
+        startDate: "4 Oct",
+        finishedDate: "16 Oct 2022",
+        comment: "OMGOMGOMGGGGGGG ITS MEE ITS MEEEE ITS MEOOOOOOOOOO :3333333333. OMG I CAN'T BELIEVE IM A MASTERPIECE NOWWWWW. Thank u so much my most precious eapm lyyyy ><. OMG IM SO LUCKYYY AINT IIIIIIIII. OMG I LOVE U SO MUCH THANK U FOR MAKING SUCH MASTERPIECE FOR ME BABEEEE :333333.",
+        by: "the boy in this portrait ><"
+    },
+    {
+        title: "Archi Sketch",
+        description: "Floating building sketch which only took a few minutes",
+        image: "https://dsm01pap007files.storage.live.com/y4mXFnrG7jDkMawrYYgN38YWCFUljDr_M7bU45mmzhh7AZD7NJELnekuJpZ0PetIyYdD2rja3tZwGc2zFjOPhMxAJ1DBeKC8UsiXtW2pQYe5wioWCWOko0KAyWJGEik4Eisq0-ILimf5q6MqZYQQhWWvciulWRSNxp4dJgGWxhh196MaYsAMMmUhGgyHO7ZDphN?width=724&height=962&cropmode=none",
+        alt: 'Portrait masterpiece of meo ^^',
+        startDate: "20 Nov 2022",
+        finishedDate: "",
+        comment: "Can u all just believe that this masterpiece is created in just a few minsss??? This is real architecture talent!!!!",
+        by: "ur top fan hhll ^^"
+    },
 ]
